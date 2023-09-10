93201c55bbf2309e69aaf0aecbf69dec6fc28d8e
emansheesha <emansha3eesha3@gmail.com>
2023-09-10 20:48:32 +0200

    props&referencetypes

---
+++ src/components/About.vue
@@ -0,0 +1,23 @@
+<!-- how to deal with props & reference type -->
+<template>
+  <div v-for="(st, i) in std" :key="i" >
+    {{ st }}
+  </div>
+  <input v-on:click="deleteArr" value="delete" type="submit"/>
+</template>
+<script>
+export default {
+  props: ["std"],
+  data() {
+    return {
+      stud: this.std,
+    };
+  },
+  methods:{
+    deleteArr(){
+        console.log('click')
+        return this.stud.pop()
+    }
+  }
+};
+</script>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,23 @@
 <template>
-
-    <div v-if="user !=null">{{ user.name }} is in home component</div>
-    <div v-else>{{ name }} is in home component</div>
+  <div v-if="user != null">{{ user.name }} is in home component</div>
+  <div v-else>{{ name }} is in home component</div>
+  <div v-for="(st, i) in students" :key="i">
+    {{ st }}
+  </div>
+  <About :std="students"/>
 </template>
 <script>
+import About from "./About.vue";
 export default {
-    data(){
-        return {
-            name:'moustafa',
-            user:JSON.parse(localStorage.getItem('user'))
-        }
-    }
-}
+  components: {
+    About,
+  },
+  data() {
+    return {
+      name: "moustafa",
+      user: JSON.parse(localStorage.getItem("user")),
+      students: ["eman", "ali", "ahmed"],
+    };
+  },
+};
 </script>
